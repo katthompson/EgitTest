f4b4cd4a0857ea3baa2b385a8527f252e29f3e35
kathy <katthompson@aspenms.com>
2012-12-31 11:15:06 -0600

    btns intents for image gallery, call log, contacts

--- EgitTest/src/com/kathy/egittest/MainActivity.java
+++ EgitTest/src/com/kathy/egittest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.kathy.egittest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,43 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button myImageGalleryButton = (Button)findViewById(R.id.myImageGalleryButton);
+		Button myCallLogButton = (Button)findViewById(R.id.myCallLogButton);
+		Button myContactBookButton = (Button)findViewById(R.id.myContactBookButton);
+		
+		//accesses Image Gallery
+		myImageGalleryButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent();
+				myIntent.setAction(Intent.ACTION_VIEW);
+				myIntent.setData(android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+				startActivity(myIntent);
+			}
+		});
+		
+		myCallLogButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent();
+				myIntent.setAction(Intent.ACTION_CALL_BUTTON);
+				startActivity(myIntent);
+			}
+		});
+		
+		myContactBookButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent();
+				myIntent.setAction(Intent.ACTION_VIEW);
+				myIntent.setData(android.provider.ContactsContract.Contacts.CONTENT_URI);
+				startActivity(myIntent);
+			}
+		});
 	}
 
 	@Override
