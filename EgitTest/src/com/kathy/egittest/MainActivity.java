02e5b9309cca84e0b1d8893f9d6cac2b365d3798
kathy <katthompson@aspenms.com>
2013-01-03 12:39:37 -0600

    added simple notification

--- EgitTest/src/com/kathy/egittest/MainActivity.java
+++ EgitTest/src/com/kathy/egittest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.kathy.egittest;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +22,7 @@ public class MainActivity extends Activity {
 		Button myImageGalleryButton = (Button)findViewById(R.id.myImageGalleryButton);
 		Button myCallLogButton = (Button)findViewById(R.id.myCallLogButton);
 		Button myContactBookButton = (Button)findViewById(R.id.myContactBookButton);
+		Button myNotificationButton = (Button) findViewById(R.id.myNotificationButton);
 		
 		//accesses Image Gallery
 		myImageGalleryButton.setOnClickListener(new Button.OnClickListener() {
@@ -48,6 +54,28 @@ public class MainActivity extends Activity {
 				myIntent.setAction(Intent.ACTION_VIEW);
 				myIntent.setData(android.provider.ContactsContract.Contacts.CONTENT_URI);
 				startActivity(myIntent);
+			}
+		});
+		
+		myNotificationButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+				Context context = MainActivity.this;
+				Intent intent = new Intent(context, MainActivity.class);
+				PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+				NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+				
+				builder.setSmallIcon(android.R.drawable.stat_notify_more)
+					.setTicker("New Notification")
+					.setContentTitle("New Message")
+					.setContentText("You have a new message.")
+					.setContentIntent(pendingIntent)
+					.setWhen(System.currentTimeMillis());
+				
+				Notification note = builder.build();			
+				nm.notify(0, note);
 			}
 		});
 	}
